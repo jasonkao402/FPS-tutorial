290f51660814125f4230a616d4a2e64579ce411a
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-03-02 19:40:50 +0800

    load script lol

---
+++ Assets/Scripts/ObjPool.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pool{
+    public string ID;
+    public GameObject blueprint;
+    public int amt;
+}
+public class ObjPool : MonoBehaviour {
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDict = new Dictionary<string, Queue<GameObject>>();
+    public Dictionary<string, GameObject> pooledObj = new Dictionary<string, GameObject>();
+    public static ObjPool Instance{get; private set;}
+    private void Reset() {
+        foreach(Pool cpool in pools)
+        {
+            Debug.Log("cleared " + cpool.ID);
+            poolDict[cpool.ID].Clear();
+        }
+    }
+    private void Awake() {
+        Instance = this;
+        foreach(Pool cpool in pools)
+        {
+            Queue<GameObject> poolq = new Queue<GameObject>();
+            for(int i = 0; i < cpool.amt; i++)
+            {
+                GameObject obj = Instantiate(cpool.blueprint, transform);
+                obj.SetActive(false);
+                poolq.Enqueue(obj);
+            }
+            poolDict.Add(cpool.ID, poolq);
+            pooledObj.Add(cpool.ID, cpool.blueprint);
+        }
+    }
+    private void addintoPool(string ID)
+    {
+        for(int i = 0; i < 30; i++)
+        {
+            GameObject obj = Instantiate(pooledObj[ID], transform);
+            obj.SetActive(false);
+            poolDict[ID].Enqueue(obj);
+        }
+    }
+    public GameObject TakePool(string ID, Vector3 p, Quaternion q)
+    {
+        if(!poolDict.ContainsKey(ID))
+        {
+            Debug.LogWarning("ID : " + ID + " not found");
+            return null;
+        }
+        else if(poolDict[ID].Count == 0)
+        {
+            Debug.LogWarning("ID : " + ID + " not available");
+            addintoPool(ID);
+            return null;
+        }
+        GameObject obj = poolDict[ID].Dequeue();
+        obj.transform.position = p;
+        obj.transform.rotation = q;
+        obj.SetactivateForAllChildren(true);
+        poolDict[ID].Enqueue(obj);
+        return obj;
+    }
+    public GameObject TakePool(string ID, Vector3 p, Quaternion q, Transform t)
+    {
+        if(!poolDict.ContainsKey(ID))
+        {
+            Debug.LogWarning("ID : " + ID + " not found");
+            return null;
+        }
+        else if(poolDict[ID].Count == 0)
+        {
+            Debug.LogWarning("ID : " + ID + " not available");
+            addintoPool(ID);
+            return null;
+        }
+        GameObject obj = poolDict[ID].Dequeue();
+        obj.transform.position = p;
+        obj.transform.rotation = q;
+        obj.transform.parent = t;
+        obj.SetactivateForAllChildren(true);
+        poolDict[ID].Enqueue(obj);
+        return obj;
+    }
+}
---
+++ Assets/Scripts/utils/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static void SetactivateForAllChildren(this GameObject gg, bool state)
+    {
+        setChildActive_recur(gg, state);
+    }
+ 
+    public static void setChildActive_recur(GameObject gg, bool state)
+    {
+        gg.SetActive(state);
+ 
+        foreach (Transform child in gg.transform)
+        {
+            setChildActive_recur(child.gameObject, state);
+        }
+    }
+}
