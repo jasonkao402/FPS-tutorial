a8990b082e8e7ab9429807a06e967510e56ee6fd
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-03-02 20:26:15 +0800

    objPool

--- Assets/Scripts/FPS_Ctrl.cs
+++ Assets/Scripts/FPS_Ctrl.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class FPS_Ctrl : MonoBehaviour {
 	Rigidbody rb;
-	public float spdCoff, jmpCoff;
+	public float spdCoff, jmpCoff, maxCheckHeight;
+	RaycastHit hitInfo;
+	public bool groundCheck;
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
 	private void Update() {
-		if(Input.GetKeyDown(KeyCode.Space)){
-			//Physics.SphereCast(transform.position , 0.2f, Vector3.down, out HumanPartDof, );
+		if(groundCheck && Input.GetKeyDown(KeyCode.Space)){
 			rb.velocity += transform.up * jmpCoff;
 		}
-		rb.AddForce((transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")) * spdCoff * Time.deltaTime, ForceMode.Impulse);
+		rb.AddForce((transform.forward * Input.GetAxisRaw("Vertical")  + 
+					 transform.right * Input.GetAxisRaw("Horizontal")) * 
+					 spdCoff * Time.deltaTime, ForceMode.VelocityChange);
+	}
+	private void FixedUpdate() {
+		groundCheck = Physics.SphereCast(transform.position, 0.2f,
+					  Vector3.down, out hitInfo, maxCheckHeight, 1<<6);
 	}
 }
--- Assets/Scripts/ObjPool.cs
+++ Assets/Scripts/ObjPool.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Pool{
+public struct Pool{
     public string ID;
     public GameObject blueprint;
     public int amt;
