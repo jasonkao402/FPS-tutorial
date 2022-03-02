9c646d653f56b1f8a607746540543ec3693463f1
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2022-03-02 16:05:01 +0800

    Initial commit

---
+++ Assets/Scripts/FPS_Ctrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Ctrl : MonoBehaviour {
+	Rigidbody rb;
+	public float spdCoff, jmpCoff;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	private void Update() {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			//Physics.SphereCast(transform.position , 0.2f, Vector3.down, out HumanPartDof, );
+			rb.velocity += transform.up * jmpCoff;
+		}
+		rb.AddForce((transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")) * spdCoff * Time.deltaTime, ForceMode.Impulse);
+	}
+}
